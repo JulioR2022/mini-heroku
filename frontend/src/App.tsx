9823f9f930e9a7b2d80e0a08bbcdc01932c7096a
JulioR2022 <221022041@aluno.unb.br>
2026-05-21 22:10:38 -0300

    ajustes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LoginPage } from './pages/login/Login';
 import RegisterPage from './pages/register/Register';
 import { ToastProvider } from './contexts/ToastContext';
-import {Dashboard} from './pages/dashboard/Dashboard';
+import Dashboard from './pages/dashboard/Dashboard';
 
 export default function App() {
   return (
